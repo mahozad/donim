0db5e79b24c8bc0398e9c5c26379f8b90fddfe69
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-09-07 16:40:18 +0430

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.0.8"
+}
+
+group = "com.pleon"
+version = "0.1"
+
+sourceSets {
+    getByName("main").java.srcDirs("src/main/kotlin")
+    getByName("test").java.srcDirs("src/test/kotlin")
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+javafx {
+    version = "12"
+    modules = listOf(
+            "javafx.base", "javafx.controls", "javafx.fxml",
+            "javafx.graphics", "javafx.media", "javafx.web",
+            "javafx.swing"
+    )
+}
+
+application {
+    mainClassName = "com.pleon.chopchop.Main"
+}
+
+/**
+ * This block is to tell gradle to use its native junit platform. We can also specify some useful
+ * things such as excluded tags, etc. here. Currently seems to be not needed.
+ */
+/*tasks.named<Test>("test") {
+    useJUnitPlatform()
+}*/
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("de.codecentric.centerdevice:javafxsvg:1.3.0")
+    implementation("net.java.dev.jna:jna:5.4.0")
+    implementation("net.java.dev.jna:jna-platform:5.4.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.3.40")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.5.0")
+    testImplementation("org.assertj:assertj-core:3.12.2")
+    testImplementation("io.mockk:mockk:1.9.3")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+//configure<JavaPluginConvention> {
+//    sourceCompatibility = JavaVersion.VERSION_12
+//}
---
+++ src/main/kotlin/com/pleon/chopchop/controller/MainController.java
@@ -0,0 +1,163 @@
+package com.pleon.chopchop.controller;
+
+import com.pleon.chopchop.CircularProgressBar;
+import com.pleon.chopchop.model.Type;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class MainController {
+
+    //@formatter:off
+    private static final long MILLIS_PER_FRAME = 5;
+    private boolean shouldMinimizeToTray = true;
+    @FXML private CircularProgressBar progressBar;
+    @FXML private Button restart;
+    @FXML private Button skip;
+    @FXML private Pane root;
+    //@formatter:on
+
+    private Type type = Type.WORK;
+    private StringProperty remainingTimeString =
+            new SimpleStringProperty(String.format("%02d:%02d",
+                    type.getLength() / 60, type.getLength() % 60));
+    private StringProperty pauseString = new SimpleStringProperty("Start");
+    private int remainingTime = type.getLength();
+    private Timeline timeline = new Timeline();
+    private float DELTA = -0.04f;
+    private boolean paused = true;
+
+    private void startTimer() {
+        remainingTime = type.getLength();
+        timeline.setCycleCount(remainingTime);
+        timeline.play();
+    }
+
+    public String getRemainingTimeString() {
+        return remainingTimeString.get();
+    }
+
+    public StringProperty remainingTimeStringProperty() {
+        return remainingTimeString;
+    }
+
+    public void setRemainingTimeString(String remainingTimeString) {
+        this.remainingTimeString.set(remainingTimeString);
+    }
+
+    public String getPauseString() {
+        return pauseString.get();
+    }
+
+    public StringProperty pauseStringProperty() {
+        return pauseString;
+    }
+
+    public void setPauseString(String pauseString) {
+        this.pauseString.set(pauseString);
+    }
+
+    public void onExitClick() {
+        /*if (shouldMinimizeToTray) {
+            ((Stage) root.getScene().getWindow()).hide();
+        } else {
+            fadeOut((Stage) root.getScene().getWindow(), event -> System.exit(0));
+        }*/
+
+        fadeOut((Stage) root.getScene().getWindow(), event -> System.exit(0));
+    }
+
+    public void minimizeApp() {
+        /*Stage.getWindows().filtered(Window::isShowing).forEach(window ->
+                ((Stage) window).setIconified(true)
+        );*/
+
+        /*fadeOut((Stage) root.getScene().getWindow(), event -> {
+            // make it opaque again, so it'll reappear properly if they click the taskbar
+            ((Stage) root.getScene().getWindow()).setOpacity(1f);
+            ((Stage) root.getScene().getWindow()).setIconified(true);
+        });*/
+
+        fadeOut((Stage) root.getScene().getWindow(), event -> {
+            // make it opaque again, so it'll reappear properly if they click the taskbar
+            ((Stage) root.getScene().getWindow()).setOpacity(1f);
+            ((Stage) root.getScene().getWindow()).hide();
+        });
+    }
+
+    private void fadeOut(Stage frame, EventHandler<ActionEvent> onFinished) {
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(MILLIS_PER_FRAME),
+                new EventHandler<>() {
+
+                    float opacity = 1f;
+
+                    @Override
+                    public void handle(ActionEvent event) {
+                        opacity = Math.max(opacity + DELTA, 0);
+                        frame.setOpacity(opacity);
+                    }
+                }
+        ));
+        timeline.setCycleCount((int) (1 / -DELTA));
+        timeline.setOnFinished(onFinished);
+        timeline.play();
+    }
+
+    public void restart() {
+        startTimer();
+    }
+
+    public void pauseResume() {
+        if (timeline.getKeyFrames().isEmpty()) {
+            timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), e -> {
+                setRemainingTimeString(String.format("%02d:%02d",
+                        remainingTime / 60, remainingTime % 60));
+                progressBar.tick((double) remainingTime / type.getLength(), type);
+                remainingTime--;
+            }));
+
+            timeline.setOnFinished(event -> {
+                type = (type == Type.WORK) ? Type.BREAK : Type.WORK;
+                startTimer();
+            });
+
+            restart.setDisable(false);
+            skip.setDisable(false);
+            startTimer();
+        }
+
+        if (paused) {
+            timeline.play();
+            setPauseString("Pause");
+        } else {
+            timeline.pause();
+            setPauseString("Resume");
+        }
+
+        paused = !paused;
+    }
+
+    public void skip() {
+        setPauseString("Pause"); // in case timer was paused while the skip pressed
+        type = (type == Type.WORK) ? Type.BREAK : Type.WORK;
+        timeline.stop();
+        startTimer();
+    }
+
+    public void changeTheme() {
+        if (root.getStyleClass().contains("dark")) {
+            root.getStyleClass().remove("dark");
+        } else {
+            root.getStyleClass().add("dark");
+        }
+    }
+}
