20f9280d78246626766865db3d107424c2e69b8d
Mahdi <legacyazd@gmail.com>
2019-09-08 19:52:57 +0430

    Make About screen movable

--- src/main/kotlin/com/pleon/chopchop/controller/AboutController.java
+++ src/main/kotlin/com/pleon/chopchop/controller/AboutController.java
@@ -7,6 +7,20 @@ import javafx.stage.Stage;
 public class AboutController {
 
     @FXML private Node root;
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    public void initialize() {
+        // Make window movable
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            root.getScene().getWindow().setX(event.getScreenX() - xOffset);
+            root.getScene().getWindow().setY(event.getScreenY() - yOffset);
+        });
+    }
 
     public void onExitClick() {
         // TODO: Fade out the stage
