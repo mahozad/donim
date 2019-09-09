84fbd71e5e68492d0186913d3ee7112d76a17120
Mahdi <legacyazd@gmail.com>
2019-09-09 17:00:18 +0430

    Remove a magic number

--- src/main/kotlin/com/pleon/chopchop/util/AnimationUtil.kt
+++ src/main/kotlin/com/pleon/chopchop/util/AnimationUtil.kt
@@ -9,16 +9,18 @@ import javafx.util.Duration
 
 object AnimationUtil {
 
+    private const val STEP = 0.01
+
     fun fadeOut(stage: Stage, onFinished: EventHandler<ActionEvent>) {
         val timeline = Timeline()
         timeline.keyFrames.add(KeyFrame(Duration.millis(1.0), object : EventHandler<ActionEvent?> {
             private var opacity = 1.0
             override fun handle(event: ActionEvent?) {
-                opacity = (opacity - 0.01).coerceAtLeast(0.0)
+                opacity = (opacity - STEP).coerceAtLeast(0.0)
                 stage.opacity = opacity
             }
         }))
-        timeline.cycleCount = (1 / 0.01).toInt()
+        timeline.cycleCount = (1 / STEP).toInt()
         timeline.onFinished = onFinished
         timeline.play()
     }
