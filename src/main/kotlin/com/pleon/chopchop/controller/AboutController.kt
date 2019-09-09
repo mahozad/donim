0cf946d79345c69e60c50ddd17cdb18f86821a6d
Mahdi <legacyazd@gmail.com>
2019-09-09 14:19:54 +0430

    Convert classes to kotlin

---
+++ src/main/kotlin/com/pleon/chopchop/controller/AboutController.kt
@@ -0,0 +1,42 @@
+package com.pleon.chopchop.controller
+
+import com.pleon.chopchop.HostServicesProvider.getHostServices
+import com.pleon.chopchop.ThemeUtil
+import javafx.beans.InvalidationListener
+import javafx.event.EventHandler
+import javafx.fxml.FXML
+import javafx.scene.control.Hyperlink
+import javafx.stage.Stage
+
+class AboutController {
+
+    @FXML
+    private lateinit var root: javafx.scene.Node
+    @FXML
+    private lateinit var link: Hyperlink
+
+    private var xOffset = 0.0
+    private var yOffset = 0.0
+
+    fun initialize() {
+        ThemeUtil.getThemeProperty().addListener(InvalidationListener { ThemeUtil.applyTheme(root) })
+        ThemeUtil.applyTheme(root)
+
+        link.setOnAction { getHostServices().showDocument(link.text) }
+
+        // Make window movable
+        root.onMousePressed = EventHandler {
+            xOffset = it.sceneX
+            yOffset = it.sceneY
+        }
+        root.onMouseDragged = EventHandler {
+            root.scene.window.x = it.screenX - xOffset
+            root.scene.window.y = it.screenY - yOffset
+        }
+    }
+
+    fun closeWindow() {
+        // TODO: Fade out the stage
+        (root.scene.window as Stage).close()
+    }
+}
