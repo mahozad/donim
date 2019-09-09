a1f39f1a9028c5c3dff9d7875b74530a1f2ec915
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-09-09 15:57:16 +0430

    Convert MainController class to kotlin, Add minor improvements

--- src/main/kotlin/com/pleon/chopchop/model/Type.java
+++ src/main/kotlin/com/pleon/chopchop/model/Type.java
@@ -2,25 +2,32 @@ package com.pleon.chopchop.model;
 
 import javafx.scene.paint.Color;
 
+import java.awt.TrayIcon.MessageType;
+
 import static javafx.scene.paint.Color.hsb;
 
 public enum Type {
 
     WORK(25 * 60,
             "You are free to use the system now",
-            hsb(0.0, 0.85, 1.0)),
-    BREAK(5 * 60, "Please take a break and return later",
-            hsb(145, 0.85, 1.0));
+            hsb(0.0, 0.85, 1.0),
+            MessageType.INFO),
+    BREAK(5 * 60,
+            "Please take a break and return later",
+            hsb(145, 0.85, 1.0),
+            MessageType.WARNING);
 
-    Type(int length, String message, Color baseColor) {
+    Type(int length, String message, Color baseColor, MessageType messageType) {
         this.length = length;
         this.message = message;
         this.baseColor = baseColor;
+        this.messageType = messageType;
     }
 
     private int length;
     private String message;
     private Color baseColor;
+    private MessageType messageType;
 
     public int getLength() {
         return length;
@@ -32,5 +39,9 @@ public enum Type {
 
     public Color getBaseColor() {
         return baseColor;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
     }
 }
