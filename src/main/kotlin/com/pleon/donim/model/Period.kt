696ee2b8f2a297e03ae566e21277d73e17bfbd5a
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-09-09 19:43:55 +0430

    Convert period length to "Duration"; Extract time formatting function

--- src/main/kotlin/com/pleon/donim/model/Period.kt
+++ src/main/kotlin/com/pleon/donim/model/Period.kt
@@ -2,19 +2,20 @@ package com.pleon.donim.model
 
 import javafx.scene.paint.Color
 import javafx.scene.paint.Color.hsb
+import javafx.util.Duration
 import java.awt.TrayIcon.MessageType
 
-enum class Period(val length: Int,
+enum class Period(val length: Duration,
                   val baseColor: Color,
                   val notification: String,
                   val notificationType: MessageType) {
 
-    WORK(25 * 60,
+    WORK(Duration.minutes(25.0),
             hsb(0.0, 0.85, 1.0),
             "You can now use the system",
             MessageType.INFO),
 
-    BREAK(5 * 60,
+    BREAK(Duration.minutes(2.0),
             hsb(145.0, 0.85, 1.0),
             "Please take a break and return later",
             MessageType.WARNING)
