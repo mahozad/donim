d6137f042f3e36b6ea3581f70464eb3b62b256dd
Mahdi <legacyazd@gmail.com>
2019-09-25 19:18:52 +0330

    Extract close window method to the base controller

--- src/main/kotlin/com/pleon/donim/controller/AboutController.kt
+++ src/main/kotlin/com/pleon/donim/controller/AboutController.kt
@@ -1,15 +1,8 @@
 package com.pleon.donim.controller
 
-import com.pleon.donim.util.AnimationUtil.FadeMode.OUT
-import com.pleon.donim.util.AnimationUtil.MoveDirection.BOTTOM
-import com.pleon.donim.util.AnimationUtil.fade
-import com.pleon.donim.util.AnimationUtil.move
 import com.pleon.donim.util.HostServicesUtil
-import javafx.event.EventHandler
 import javafx.fxml.FXML
 import javafx.scene.control.Hyperlink
-import javafx.stage.Stage
-import javafx.util.Duration
 
 class AboutController : BaseController() {
 
@@ -21,10 +14,5 @@ class AboutController : BaseController() {
         makeWindowMovable()
     }
 
-    fun closeWindow() {
-        val delay = Duration.millis(0.0)
-        val duration = Duration.millis(100.0)
-        fade(OUT, root, delay, duration, EventHandler { (root.scene.window as Stage).close() })
-        move(BOTTOM, root.scene.window, delay, duration)
-    }
+    fun close() = closeWindow(false)
 }
