21e25c775e3192a3780560009676ea995fbc8fac
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2019-09-27 00:34:29 +0330

    Refactor code

--- src/main/kotlin/com/pleon/donim/controller/MainController.kt
+++ src/main/kotlin/com/pleon/donim/controller/MainController.kt
@@ -62,6 +62,7 @@ class MainController : BaseController() {
         createTrayIcon()
         makeTrayIconAnimatable()
         makeWindowMovable()
+        setupTimeline()
     }
 
     private fun createTrayIcon() {
@@ -116,6 +117,19 @@ class MainController : BaseController() {
         }))
     }
 
+    private fun setupTimeline() {
+        timeline.keyFrames.add(KeyFrame(Duration.seconds(1.0), EventHandler {
+            setRemainingTimeString(format(remainingTime))
+            progressBar.tick(remainingTime.toMillis() / period.length.toMillis(), period.baseColor)
+            remainingTime = remainingTime.subtract(Duration.seconds(1.0))
+        }))
+
+        timeline.setOnFinished {
+            period = if (period == WORK) BREAK else WORK
+            startTimer(true)
+        }
+    }
+
     private fun newMenuItem(title: String, listener: (java.awt.event.ActionEvent) -> Unit): MenuItem {
         return MenuItem(title).apply { addActionListener(listener) }
     }
@@ -136,15 +150,15 @@ class MainController : BaseController() {
 
     private fun startTimer(shouldNotify: Boolean) {
         playIcon.content = "m 8,18.1815 c 1.1,0 2,-0.794764 2,-1.766143 V 7.5846429 C 10,6.6132643 9.1,5.8185 8,5.8185 6.9,5.8185 6,6.6132643 6,7.5846429 V 16.415357 C 6,17.386736 6.9,18.1815 8,18.1815 Z M 14,7.5846429 v 8.8307141 c 0,0.971379 0.9,1.766143 2,1.766143 1.1,0 2,-0.794764 2,-1.766143 V 7.5846429 C 18,6.6132643 17.1,5.8185 16,5.8185 c -1.1,0 -2,0.7947643 -2,1.7661429 z"
-        trayAnimation.play()
         remainingTime = period.length
         timeline.cycleCount = remainingTime.toSeconds().toInt()
+        timeline.play()
+        trayAnimation.play()
+        trayIcon.toolTip = "Donim: $period"
         if (shouldNotify) {
             trayIcon.displayMessage(period.toString(), period.notification, period.notificationType)
             beep.play()
         }
-        trayIcon.toolTip = "Donim: $period"
-        timeline.play()
     }
 
     fun close() = closeWindow(true)
@@ -168,34 +182,16 @@ class MainController : BaseController() {
     }
 
     fun pauseResume() {
-        if (timeline.keyFrames.isEmpty()) {
-            timeline.keyFrames.add(KeyFrame(Duration.seconds(1.0), EventHandler {
-                setRemainingTimeString(format(remainingTime))
-                progressBar.tick(remainingTime.toMillis() / period.length.toMillis(), period.baseColor)
-                remainingTime = remainingTime.subtract(Duration.seconds(1.0))
-            }))
-
-            timeline.setOnFinished {
-                period = if (period == WORK) BREAK else WORK
-                startTimer(true)
-            }
-
-            restart.isDisable = false
+        paused = !paused
+        if (paused) {
+            // tray animation is paused in its keyframe event handler
+            timeline.pause()
+            playIcon.content = "M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"
+        } else {
             skip.isDisable = false
+            restart.isDisable = false
             startTimer(false)
         }
-
-        if (paused) {
-            timeline.play()
-            trayAnimation.play()
-            playIcon.content = "m 8,18.1815 c 1.1,0 2,-0.794764 2,-1.766143 V 7.5846429 C 10,6.6132643 9.1,5.8185 8,5.8185 6.9,5.8185 6,6.6132643 6,7.5846429 V 16.415357 C 6,17.386736 6.9,18.1815 8,18.1815 Z M 14,7.5846429 v 8.8307141 c 0,0.971379 0.9,1.766143 2,1.766143 1.1,0 2,-0.794764 2,-1.766143 V 7.5846429 C 18,6.6132643 17.1,5.8185 16,5.8185 c -1.1,0 -2,0.7947643 -2,1.7661429 z"
-        } else {
-            timeline.pause()
-            // trayAnimation is paused in its keyframe event handler
-            playIcon.content = "M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"
-        }
-
-        paused = !paused
     }
 
     private fun format(duration: Duration): String {
