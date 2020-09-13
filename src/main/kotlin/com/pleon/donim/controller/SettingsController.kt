719b7228d9ee7437ba282bd9056c728743f13f8f
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-14 02:35:56 +0430

    WIP - Add initial impl of settings screen

---
+++ src/main/kotlin/com/pleon/donim/controller/SettingsController.kt
@@ -0,0 +1,10 @@
+package com.pleon.donim.controller
+
+import com.pleon.donim.hostServicesInstance
+import javafx.fxml.FXML
+import javafx.scene.control.Hyperlink
+
+class SettingsController : BaseController() {
+
+    fun close() = closeWindow(false)
+}
