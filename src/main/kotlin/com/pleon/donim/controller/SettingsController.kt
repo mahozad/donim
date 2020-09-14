87fd8c9530de02f4040ecec4b95f5eae60ece144
Mahdi Hosseinzadeh <legacyazd@gmail.com>
2020-09-14 22:37:53 +0430

    Make settings show the last saved settings and update on action

--- src/main/kotlin/com/pleon/donim/controller/SettingsController.kt
+++ src/main/kotlin/com/pleon/donim/controller/SettingsController.kt
@@ -5,14 +5,40 @@ import com.pleon.donim.util.PersistentSettings
 import javafx.beans.value.ObservableStringValue
 import javafx.fxml.FXML
 import javafx.scene.control.TextField
+import javafx.scene.control.ToggleButton
 
 class SettingsController : BaseController() {
 
     @FXML private lateinit var focusDuration: TextField
     @FXML private lateinit var breakDuration: TextField
+    @FXML private lateinit var toggleTheme: ToggleButton
 
     override fun initialize() {
         super.initialize()
+        setInitialSettingValues()
+        setupListeners()
+    }
+
+    private fun setInitialSettingValues() {
+        focusDuration.text = PersistentSettings.get("focus-duration")
+        breakDuration.text = PersistentSettings.get("break-duration")
+        if (PersistentSettings.get("theme") == DecorationUtil.Theme.DARK.name) {
+            toggleTheme.isSelected = true
+            toggleTheme.text = "Switch to light theme"
+        } else {
+            toggleTheme.isSelected = false
+            toggleTheme.text = "Switch to dark theme"
+        }
+    }
+
+    private fun setupListeners() {
+        toggleTheme.selectedProperty().addListener { observable, oldValue, newValue ->
+            if (newValue == true) {
+                toggleTheme.text = "Switch to light theme"
+            } else {
+                toggleTheme.text = "Switch to dark theme"
+            }
+        }
         focusDuration.textProperty().addListener { _, _, newValue ->
             PersistentSettings.set("focus-duration", newValue)
         }
