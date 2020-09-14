9f80cfa7fad05e5c0b6d7e80b650347455e988bd
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-14 15:49:13 +0430

    Provide accessors for getting value of durations as observables

--- src/main/kotlin/com/pleon/donim/controller/SettingsController.kt
+++ src/main/kotlin/com/pleon/donim/controller/SettingsController.kt
@@ -1,9 +1,23 @@
 package com.pleon.donim.controller
 
 import com.pleon.donim.util.DecorationUtil
+import javafx.beans.value.ObservableStringValue
+import javafx.fxml.FXML
+import javafx.scene.control.TextField
 
 class SettingsController : BaseController() {
 
+    @FXML private lateinit var focusDuration: TextField
+    @FXML private lateinit var breakDuration: TextField
+
     fun close() = closeWindow(false)
     fun toggleTheme() = DecorationUtil.toggleTheme()
+
+    fun getObservableFocusDuration(): ObservableStringValue {
+        return focusDuration.textProperty()
+    }
+
+    fun getObservableBreakDuration(): ObservableStringValue {
+        return breakDuration.textProperty()
+    }
 }
