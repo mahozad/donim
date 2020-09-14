1b47f65f7524b7fb381c6cb1d642c3e4f237c954
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-14 17:57:43 +0430

    Add an observable accessor for properties in Settings

--- src/main/kotlin/com/pleon/donim/util/PersistentSettings.kt
+++ src/main/kotlin/com/pleon/donim/util/PersistentSettings.kt
@@ -1,6 +1,8 @@
 package com.pleon.donim.util
 
 import com.pleon.donim.exception.SettingNotFoundException
+import javafx.collections.FXCollections
+import javafx.collections.ObservableMap
 import java.io.FileInputStream
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
@@ -12,6 +14,7 @@ object PersistentSettings : Settings {
 
     private val properties = Properties()
     private const val FILE_NAME = "app-settings.properties"
+    private val observableProperties = FXCollections.observableMap(properties)
 
     init {
         try {
@@ -24,8 +27,9 @@ object PersistentSettings : Settings {
     }
 
     override fun set(name: String, value: String) = try {
+        observableProperties[name] = value
+        properties[name] = value
         val outputStream = FileOutputStream(FILE_NAME)
-        properties.setProperty(name, value)
         properties.store(outputStream, null)
         outputStream.close()
     } catch (e: Exception) {
@@ -34,5 +38,9 @@ object PersistentSettings : Settings {
 
     override fun get(name: String): String {
         return properties.getProperty(name) ?: throw SettingNotFoundException()
+    }
+
+    override fun getObservableProperties(): ObservableMap<String, String> {
+        return observableProperties as ObservableMap<String, String>
     }
 }
--- src/main/kotlin/com/pleon/donim/util/Settings.kt
+++ src/main/kotlin/com/pleon/donim/util/Settings.kt
@@ -1,8 +1,12 @@
 package com.pleon.donim.util
 
+import javafx.collections.ObservableMap
+
 interface Settings {
 
     fun get(name: String): String
 
     fun set(name: String, value: String)
+
+    fun getObservableProperties(): ObservableMap<String, String>
 }
