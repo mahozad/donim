5cc1975509f6439ca5b0b750353163f0704327be
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-14 18:29:22 +0430

    Add initial impl. of using periods specified in settings

--- src/main/kotlin/com/pleon/donim/model/Period.kt
+++ src/main/kotlin/com/pleon/donim/model/Period.kt
@@ -1,21 +1,24 @@
 package com.pleon.donim.model
 
+import com.pleon.donim.util.PersistentSettings
 import javafx.scene.paint.Color
 import javafx.scene.paint.Color.hsb
 import javafx.util.Duration
 import java.awt.TrayIcon.MessageType
 
-enum class Period(val length: Duration,
+enum class Period(var length: Duration,
                   val baseColor: Color,
                   val notification: String,
                   val notificationType: MessageType) {
 
-    WORK(Duration.minutes(25.0),
+    // FIXME: Handle empty or not existent cases
+    WORK(Duration.minutes(PersistentSettings.get("focus-duration").toDouble()),
             hsb(0.0, 0.85, 1.0),
             "You can now use the system",
             MessageType.INFO),
 
-    BREAK(Duration.minutes(2.0),
+    // FIXME: Handle empty or not existent cases
+    BREAK(Duration.minutes(PersistentSettings.get("break-duration").toDouble()),
             hsb(100.0, 0.85, 1.0),
             "Please take a break and return later",
             MessageType.WARNING)
