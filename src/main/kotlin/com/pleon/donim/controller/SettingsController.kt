e534e43c354a2103f82e778f2b3523a9205fa097
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-14 17:07:42 +0430

    Stores the new durations (every change) in settings

--- src/main/kotlin/com/pleon/donim/controller/SettingsController.kt
+++ src/main/kotlin/com/pleon/donim/controller/SettingsController.kt
@@ -1,6 +1,7 @@
 package com.pleon.donim.controller
 
 import com.pleon.donim.util.DecorationUtil
+import com.pleon.donim.util.PersistentSettings
 import javafx.beans.value.ObservableStringValue
 import javafx.fxml.FXML
 import javafx.scene.control.TextField
@@ -9,6 +10,16 @@ class SettingsController : BaseController() {
 
     @FXML private lateinit var focusDuration: TextField
     @FXML private lateinit var breakDuration: TextField
+
+    override fun initialize() {
+        super.initialize()
+        focusDuration.textProperty().addListener { _, _, newValue ->
+            PersistentSettings.set("focus-duration", newValue)
+        }
+        breakDuration.textProperty().addListener { _, _, newValue ->
+            PersistentSettings.set("break-duration", newValue)
+        }
+    }
 
     fun close() = closeWindow(false)
     fun toggleTheme() = DecorationUtil.toggleTheme()
