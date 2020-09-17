654cb426ad7d3e80850387bc499863c21cb15bba
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-17 20:40:11 +0430

    Change the path of the stored file to os-specific user data path

--- src/main/kotlin/com/pleon/donim/util/PersistentSettings.kt
+++ src/main/kotlin/com/pleon/donim/util/PersistentSettings.kt
@@ -12,24 +12,38 @@ import java.util.*
 
 object PersistentSettings : Settings {
 
-    private val properties = Properties()
     private const val FILE_NAME = "preferences.properties"
+    private val fileDirectory = getOsSpecificDirectory()
+    private val properties = Properties()
     private val observableProperties = FXCollections.observableMap(properties)
 
     init {
         try {
-            properties.load(FileInputStream(FILE_NAME))
+            properties.load(FileInputStream("$fileDirectory$FILE_NAME"))
         } catch (e: FileNotFoundException) {
-            Files.createFile(Path.of(FILE_NAME))
+            Files.createDirectories(Path.of(fileDirectory))
+            Files.createFile(Path.of(fileDirectory, FILE_NAME))
         } catch (e: Exception) {
             e.printStackTrace()
+        }
+    }
+
+    /**
+     * See https://stackoverflow.com/q/1198911/
+     */
+    private fun getOsSpecificDirectory(): String {
+        val osName = System.getProperty("os.name").toLowerCase()
+        return if (osName.contains("windows")) {
+            System.getenv("AppData") + "/Donim/"
+        } else {
+            System.getProperty("user.home") + "/Donim/"
         }
     }
 
     override fun set(name: String, value: String) = try {
         observableProperties[name] = value
         properties[name] = value
-        val outputStream = FileOutputStream(FILE_NAME)
+        val outputStream = FileOutputStream("$fileDirectory$FILE_NAME")
         properties.store(outputStream, null)
         outputStream.close()
     } catch (e: Exception) {
