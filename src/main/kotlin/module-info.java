948fd6088bdabd9f30bf6a8a8f6aa23914918dc9
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-09-19 13:37:31 +0430

    Remove an unneeded directive

--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -11,8 +11,7 @@ module donim.application {
 
     requires filters; // The com.jhlabs:filters library
 
-    opens com.pleon.donim;
-    opens com.pleon.donim.controller;
     exports com.pleon.donim;
     exports com.pleon.donim.node;
+    opens com.pleon.donim.controller;
 }
