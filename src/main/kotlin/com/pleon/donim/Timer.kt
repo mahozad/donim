f20d4bc0f200d55bb77f71b813c2735d6b15dba3
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-07 21:20:59 +0330

    Refactor start() and stop() methods

--- src/main/kotlin/com/pleon/donim/Timer.kt
+++ src/main/kotlin/com/pleon/donim/Timer.kt
@@ -1,5 +1,7 @@
 package com.pleon.donim
 
+import javafx.animation.KeyFrame
+import javafx.animation.Timeline
 import javafx.beans.property.ReadOnlyObjectWrapper
 import java.time.Duration
 
@@ -8,17 +10,22 @@ class Timer(private val duration: Duration) {
     var isStarted = false
         private set
     private var remainingTimeProperty = ReadOnlyObjectWrapper<Duration>()
+    private val timeline = Timeline()
     private var remainingTime = duration
 
     fun remainingTimeProperty() = remainingTimeProperty.readOnlyProperty
 
     fun start() {
+        timeline.keyFrames.add(KeyFrame(javafx.util.Duration.seconds(1.0), {
+            remainingTime = remainingTime.minusSeconds(1)
+            remainingTimeProperty.set(remainingTime)
+        }))
+        timeline.play()
         isStarted = true
-        remainingTime = remainingTime.minusSeconds(1)
-        remainingTimeProperty.set(remainingTime)
     }
 
     fun stop() {
+        timeline.pause()
         isStarted = false
     }
 
