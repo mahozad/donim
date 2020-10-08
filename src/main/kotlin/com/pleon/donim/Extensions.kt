6df9b9c97289a5360fcd599bc54a7dd6b290a4a3
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-08 15:51:44 +0330

    Refactor code

--- src/main/kotlin/com/pleon/donim/Extensions.kt
+++ src/main/kotlin/com/pleon/donim/Extensions.kt
@@ -2,6 +2,7 @@ package com.pleon.donim
 
 import javafx.util.Duration
 
+val Duration.length: Int get() = toMillis().toInt()
 operator fun Duration.plus(other: Duration): Duration = add(other)
 operator fun Duration.minus(other: Duration): Duration = subtract(other)
 operator fun Duration.times(num: Int): Duration = multiply(num.toDouble())
--- src/main/kotlin/com/pleon/donim/Timer.kt
+++ src/main/kotlin/com/pleon/donim/Timer.kt
@@ -3,23 +3,29 @@ package com.pleon.donim
 import javafx.animation.Animation.Status.RUNNING
 import javafx.animation.KeyFrame
 import javafx.animation.Timeline
+import javafx.beans.property.ReadOnlyObjectProperty
 import javafx.beans.property.ReadOnlyObjectWrapper
 import javafx.util.Duration
 
-class Timer(private val duration: Duration, private val updateRate: Duration) {
+class Timer(private val duration: Duration,
+            private val updateRate: Duration) {
 
     val isRunning get() = timeline.status == RUNNING
     private val remainingTimeProperty = ReadOnlyObjectWrapper(duration)
     private val timeline = Timeline()
 
     init {
-        timeline.keyFrames.add(KeyFrame(updateRate, { remainingTimeProperty.value -= updateRate }))
+        timeline.keyFrames.add(
+                KeyFrame(updateRate, { remainingTimeProperty.value -= updateRate })
+        )
     }
 
-    fun remainingTimeProperty() = remainingTimeProperty.readOnlyProperty
+    fun remainingTimeProperty(): ReadOnlyObjectProperty<Duration> {
+        return remainingTimeProperty.readOnlyProperty
+    }
 
     fun start() {
-        timeline.cycleCount = duration.toMillis().toInt() / updateRate.toMillis().toInt()
+        timeline.cycleCount = duration.length / updateRate.length
         timeline.play()
     }
 
