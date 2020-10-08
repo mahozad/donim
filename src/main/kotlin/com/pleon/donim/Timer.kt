041f93dde512efb9039402e2f273ef2df15e6f9f
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-08 14:09:55 +0330

    Add times and div operator functions for javafx Duration class

--- src/main/kotlin/com/pleon/donim/Timer.kt
+++ src/main/kotlin/com/pleon/donim/Timer.kt
@@ -35,5 +35,9 @@ class Timer(duration: Duration, updateRate: Duration) {
     }
 }
 
-operator fun Duration.minus(other: Duration): Duration = subtract(other)
 operator fun Duration.plus(other: Duration): Duration = add(other)
+operator fun Duration.minus(other: Duration): Duration = subtract(other)
+operator fun Duration.times(num: Int): Duration = multiply(num.toDouble())
+operator fun Duration.times(num: Double): Duration = multiply(num)
+operator fun Duration.div(num: Int): Duration = divide(num.toDouble())
+operator fun Duration.div(num: Double): Duration = divide(num)
