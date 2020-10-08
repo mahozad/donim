3ed80355d785f4cc009f8f63a41a1630f2d2647d
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-08 15:37:51 +0330

    Move extension functions to a dedicated file

---
+++ src/main/kotlin/com/pleon/donim/Extensions.kt
@@ -0,0 +1,10 @@
+package com.pleon.donim
+
+import javafx.util.Duration
+
+operator fun Duration.plus(other: Duration): Duration = add(other)
+operator fun Duration.minus(other: Duration): Duration = subtract(other)
+operator fun Duration.times(num: Int): Duration = multiply(num.toDouble())
+operator fun Duration.times(num: Double): Duration = multiply(num)
+operator fun Duration.div(num: Int): Duration = divide(num.toDouble())
+operator fun Duration.div(num: Double): Duration = divide(num)
--- src/main/kotlin/com/pleon/donim/Timer.kt
+++ src/main/kotlin/com/pleon/donim/Timer.kt
@@ -32,10 +32,3 @@ class Timer(private val duration: Duration, private val updateRate: Duration) {
         timeline.playFromStart()
     }
 }
-
-operator fun Duration.plus(other: Duration): Duration = add(other)
-operator fun Duration.minus(other: Duration): Duration = subtract(other)
-operator fun Duration.times(num: Int): Duration = multiply(num.toDouble())
-operator fun Duration.times(num: Double): Duration = multiply(num)
-operator fun Duration.div(num: Int): Duration = divide(num.toDouble())
-operator fun Duration.div(num: Double): Duration = divide(num)
