8207d0b56e14bc520df864d76c0a86bef3090b5e
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-20 21:44:18 +0330

    Add an alternative low-level image tinting function

--- src/main/kotlin/com/pleon/donim/util/ImageUtil.kt
+++ src/main/kotlin/com/pleon/donim/util/ImageUtil.kt
@@ -1,6 +1,7 @@
 package com.pleon.donim.util
 
 import com.jhlabs.image.HSBAdjustFilter
+import javafx.scene.paint.Color
 import java.awt.RenderingHints.KEY_INTERPOLATION
 import java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR
 import java.awt.image.BufferedImage
@@ -21,6 +22,23 @@ object ImageUtil {
         val hsbFilter = HSBAdjustFilter().apply { setHFactor(hueFactor.toFloat()) }
         val destination = hsbFilter.createCompatibleDestImage(image, null)
         return hsbFilter.filter(image, destination)
+    }
+
+    /**
+     * This does not use external libraries like jhlabs (which is not maintained and id about 300 KiB)
+     */
+    fun tintImageManually(image: BufferedImage, hueFactor: Double): BufferedImage {
+        for (i in 0 until image.width) {
+            for (j in 0 until image.height) {
+                val originalColor = java.awt.Color(image.getRGB(i, j), true)
+                val originalColorFX = Color(originalColor.red.toDouble() / 255.0, originalColor.green.toDouble() / 255.0, originalColor.blue.toDouble() / 255.0, originalColor.alpha / 255.0)
+                val newColorFX =/* originalColorFX.interpolate(AQUA, 0.5) */originalColorFX.deriveColor(hueFactor*365, 1.0, 1.0, 1.0)
+                val newColor = java.awt.Color(newColorFX.red.toFloat(), newColorFX.green.toFloat(), newColorFX.blue.toFloat(), newColorFX.opacity.toFloat())
+                // val rgb = ((newColorFX.opacity * 255).toInt() shl 24) + ((newColorFX.red * 255).toInt() shl 16) + ((newColorFX.green * 255).toInt() shl 8) + (newColorFX.blue * 255).toInt()
+                image.setRGB(i, j, newColor.rgb)
+            }
+        }
+        return image
     }
 
     // NOTE:
