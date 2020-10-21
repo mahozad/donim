a4ab9392c1dbd5620c0677e84728222d904469fb
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-21 11:23:14 +0330

    Remove jhlabs library and its image tinting functions

--- src/main/kotlin/com/pleon/donim/util/ImageUtil.kt
+++ src/main/kotlin/com/pleon/donim/util/ImageUtil.kt
@@ -1,12 +1,10 @@
 package com.pleon.donim.util
 
-import com.jhlabs.image.HSBAdjustFilter
 import java.awt.Color.HSBtoRGB
 import java.awt.Color.RGBtoHSB
 import java.awt.RenderingHints.KEY_INTERPOLATION
 import java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR
 import java.awt.image.BufferedImage
-
 
 object ImageUtil {
 
@@ -21,29 +19,10 @@ object ImageUtil {
     }
 
     fun tintImage(image: BufferedImage, hueFactor: Double): BufferedImage {
-        val hsbFilter = HSBAdjustFilter().apply { setHFactor(hueFactor.toFloat()) }
-        val destination = hsbFilter.createCompatibleDestImage(image, null)
-        return hsbFilter.filter(image, destination)
+        return tintImage(image, hueFactor.toFloat())
     }
 
-    // /**
-    //  * This does not use external libraries like jhlabs (which is not maintained and id about 300 KiB)
-    //  */
-    // fun tintImage2(image: BufferedImage, hueFactor: Double): BufferedImage {
-    //     for (i in 0 until image.width) {
-    //         for (j in 0 until image.height) {
-    //             val originalColor = java.awt.Color(image.getRGB(i, j), true)
-    //             val originalColorFX = Color(originalColor.red.toDouble() / 255.0, originalColor.green.toDouble() / 255.0, originalColor.blue.toDouble() / 255.0, originalColor.alpha / 255.0)
-    //             val newColorFX =/* originalColorFX.interpolate(AQUA, 0.5) */originalColorFX.deriveColor(hueFactor * 360, 1.0, 1.0, 1.0)
-    //             val newColor = java.awt.Color(newColorFX.red.toFloat(), newColorFX.green.toFloat(), newColorFX.blue.toFloat(), newColorFX.opacity.toFloat())
-    //             // val rgb = ((newColorFX.opacity * 255).toInt() shl 24) + ((newColorFX.red * 255).toInt() shl 16) + ((newColorFX.green * 255).toInt() shl 8) + (newColorFX.blue * 255).toInt()
-    //             image.setRGB(i, j, newColor.rgb)
-    //         }
-    //     }
-    //     return image
-    // }
-
-    fun tintImage3(image: BufferedImage, hueFactor: Float): BufferedImage {
+    fun tintImage(image: BufferedImage, hueFactor: Float): BufferedImage {
         for (i in 0 until image.width) {
             for (j in 0 until image.height) {
                 val rgb = image.getRGB(i, j)
