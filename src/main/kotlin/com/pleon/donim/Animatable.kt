c4e3a879787b13813ef694be4b234e050f7c0351
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-22 16:39:59 +0330

    Rename interface ColorAnimatable to Animatable

--- src/main/kotlin/com/pleon/donim/Animatable.kt
+++ src/main/kotlin/com/pleon/donim/Animatable.kt
@@ -3,7 +3,7 @@ package com.pleon.donim
 import javafx.scene.paint.Color
 import javafx.util.Duration
 
-interface ColorAnimatable {
+interface Animatable {
     fun startAnimation(duration: Duration, startColor: Color, endColor: Color)
     fun stopAnimation(graceful: Boolean = true)
     fun resetAnimation(graceful: Boolean = true)
