1aebc84946b217c7f67a62783b6875bf97a1bb38
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-24 18:16:25 +0330

    Create class Tray implementing Animatable

---
+++ src/main/kotlin/com/pleon/donim/node/Tray.kt
@@ -0,0 +1,23 @@
+package com.pleon.donim.node
+
+import com.pleon.donim.Animatable
+import javafx.util.Duration
+
+class Tray : Animatable {
+
+    override fun startAnimation() {
+        TODO("Not yet implemented")
+    }
+
+    override fun pauseAnimation() {
+        TODO("Not yet implemented")
+    }
+
+    override fun resetAnimation(properties: Animatable.AnimationProperties) {
+        TODO("Not yet implemented")
+    }
+
+    override fun endAnimation(onEnd: () -> Unit, graceful: Boolean, graceDuration: Duration) {
+        TODO("Not yet implemented")
+    }
+}
