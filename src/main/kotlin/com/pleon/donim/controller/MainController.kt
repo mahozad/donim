fabd04c234fb7096cee8928c04050e420f34f251
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-10-25 12:11:50 +0330

    Remove an unneeded TODO

--- src/main/kotlin/com/pleon/donim/controller/MainController.kt
+++ src/main/kotlin/com/pleon/donim/controller/MainController.kt
@@ -158,14 +158,6 @@ class MainController : BaseController() {
     fun pauseResume() {
         paused = !paused
         if (paused) {
-
-
-
-            // TODO:
-            // tray animation is paused in its keyframe event handler
-            // if (trayFrameNumber == 0) trayIcon.image = trayImage.tint(0.0)
-
-
             playIcon.content = "M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"
             time.pauseAnimation()
             tray.pauseAnimation()
