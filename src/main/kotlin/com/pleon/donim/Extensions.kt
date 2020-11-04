26e3ce28fdd6f9345fce97bf6661dd0b54324d4c
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-11-04 15:59:23 +0330

    Reformat code

--- src/main/kotlin/com/pleon/donim/Extensions.kt
+++ src/main/kotlin/com/pleon/donim/Extensions.kt
@@ -3,11 +3,19 @@ package com.pleon.donim
 import javafx.util.Duration
 
 val Duration.length: Int get() = toMillis().toInt()
+
 operator fun Duration.plus(other: Duration): Duration = add(other)
+
 operator fun Duration.minus(other: Duration): Duration = subtract(other)
+
 operator fun Duration.times(num: Int): Duration = multiply(num.toDouble())
+
 operator fun Duration.times(num: Double): Duration = multiply(num)
+
 operator fun Duration.div(num: Int): Duration = divide(num.toDouble())
+
 operator fun Duration.div(num: Double): Duration = divide(num)
+
 operator fun Duration.div(other: Duration): Double = toMillis() / other.toMillis()
+
 operator fun Duration.rem(other: Duration): Double = toMillis() % other.toMillis()
