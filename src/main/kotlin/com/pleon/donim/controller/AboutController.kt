537bd984042b6603008b5e5850e7478d6f4ee509
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-11-04 14:43:18 +0330

    Refactor and simplify AboutController

--- src/main/kotlin/com/pleon/donim/controller/AboutController.kt
+++ src/main/kotlin/com/pleon/donim/controller/AboutController.kt
@@ -2,17 +2,10 @@ package com.pleon.donim.controller
 
 import com.pleon.donim.APP_SOURCE_PAGE_URI
 import com.pleon.donim.hostServicesInstance
-import javafx.fxml.FXML
-import javafx.scene.control.Hyperlink
 
 class AboutController : BaseController() {
 
-    @FXML private lateinit var link: Hyperlink
-
-    override fun initialize() {
-        super.initialize()
-        link.setOnAction { hostServicesInstance.showDocument(APP_SOURCE_PAGE_URI) }
-    }
+    fun viewSourceCode() = hostServicesInstance.showDocument(APP_SOURCE_PAGE_URI)
 
     fun close() = closeWindow(false)
 }
